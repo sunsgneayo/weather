736a49cf13c8ce1823f0f01e5b8ffb029d898420
usone <1129371285@qq,com>
2022-05-17 11:11:02 +0800

    0.0.5

--- test/test.php
+++ test/test.php
@@ -1,10 +1,10 @@
 <?php
 
 
-require_once './vendor/autoload.php';
+//require_once './vendor/autoload.php';
 
 
 //$response = \tinywan\Weather::liveWeather("重庆");
-$response = sunsgne\Weather::liveWeather("重庆");
+$response = \sunsgne\Weather::liveWeather("重庆");
 
 var_dump($response);
